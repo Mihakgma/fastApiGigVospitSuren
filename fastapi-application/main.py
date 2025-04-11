22ac51f52fcfad1ab713b0af99b040dad3b2c70d
Tabakaev Mihail <mihakgma86@hotmail.com>
2025-04-11 23:27:06 +0700

    in main.py file added lifespan func to control startup and shutdown actions for application, app in that file renamed to main_app

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,30 +1,45 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from loguru import logger
 
 from api import router as api_router
+from core.models import db_helper
 from core.config import settings
 
-app = FastAPI()
-app.include_router(
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    print("dispose engine")
+    await db_helper.dispose()
+
+
+main_app = FastAPI(
+    lifespan=lifespan,
+    )
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix
 )
 
 
-@app.get("/")
+@main_app.get("/")
 async def root():
     return {"message": "Вас приветствует приложение "}
 
 
-@app.get("/hello/{name}")
+@main_app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
 if __name__ == "__main__":
     logger.info("Starting server...")
-    uvicorn.run("main:app",
+    uvicorn.run("main:main_app",
                 host=settings.run.host,
                 port=settings.run.port,
                 reload=True,
